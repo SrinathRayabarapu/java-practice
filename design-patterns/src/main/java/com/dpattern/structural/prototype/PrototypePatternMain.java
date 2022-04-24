325dd434abdf8f9f314013187b20cd70f6b5a6c3
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-04-24 16:15:34 +0530

    EPAM puzzle updated

--- design-patterns/src/main/java/com/dpattern/structural/prototype/PrototypePatternMain.java
+++ design-patterns/src/main/java/com/dpattern/structural/prototype/PrototypePatternMain.java
@@ -5,9 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @EPAM
+ */
 public class PrototypePatternMain {
 
     public static void main(String[] args) throws CloneNotSupportedException {
+
         ColorStore colorStore = new ColorStore();
         Color blue = colorStore.getColor("blue");
         Color red = colorStore.getColor("red");
