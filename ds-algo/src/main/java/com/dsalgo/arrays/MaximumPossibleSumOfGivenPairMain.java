5ccd4dd934aea11c24ee22dc389714e9bca49992
Srinath Rayabarapu <naath.r@gmail.com>
2022-09-08 12:53:26 +0530

    Generating the QR code and writing to a file location

---
+++ ds-algo/src/main/java/com/dsalgo/arrays/MaximumPossibleSumOfGivenPairMain.java
@@ -0,0 +1,14 @@
+package com.dsalgo.arrays;
+
+/**
+ *
+ */
+public class MaximumPossibleSumOfGivenPairMain {
+
+    public static void main(String[] args) {
+
+        // TODO
+
+    }
+
+}
---
+++ proj_generating-qr-code/src/main/java/com/proj/qrcodegenerator/GenerateQRCodeMain.java
@@ -0,0 +1,38 @@
+package com.proj.qrcodegenerator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@Slf4j
+public class GenerateQRCodeMain {
+
+    public static void main(String[] args) throws WriterException, IOException {
+
+        String qrCodeToBeGeneratedURL = "https://www.youtube.com/watch?v=9j-i-o7vx1c&ab_channel=TechStack9";
+        log.info("Generating QR Code for : {}", qrCodeToBeGeneratedURL);
+
+        String charset = "UTF-8";
+
+        // get the current project path
+        String pathToSaveQRCodes = System.getProperty("user.dir") + "/proj_generating-qr-code/src/main/resources/qrcodes/TechStack9.png";
+
+        generateQRCode(qrCodeToBeGeneratedURL, pathToSaveQRCodes, 500, 500);
+
+        log.info("QR Code successfully generated!");
+    }
+
+    private static void generateQRCode(String qrCodeToBeGeneratedURL, String pathToSaveQRCodes, int width, int height) throws WriterException, IOException {
+
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(qrCodeToBeGeneratedURL, BarcodeFormat.QR_CODE, width, height);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, pathToSaveQRCodes.substring(pathToSaveQRCodes.lastIndexOf(".")+1), Paths.get(pathToSaveQRCodes));
+    }
+
+}
