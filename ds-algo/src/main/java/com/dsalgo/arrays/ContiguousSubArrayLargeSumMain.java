b61b1711b8ec10e315f2b9f7a438febf00ebc502
Srinath Rayabarapu <naath.r@gmail.com>
2022-11-08 18:58:37 +0530

    Scaler classes exercises

---
+++ ds-algo/src/main/java/com/dsalgo/arrays/ContiguousSubArrayLargeSumMain.java
@@ -0,0 +1,35 @@
+package com.dsalgo.arrays;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Find the contiguous non-empty sub-array within an array A of length N, with the largest sum
+ * <p>
+ * # L1 - Medium - InterviewQuestion
+ */
+@Slf4j
+public class ContiguousSubArrayLargeSumMain {
+
+    public static void main(String[] args) {
+
+        int[] inputArray = {1, -2, 3, 4, -10};
+
+        int largeSum = findLargestSubArray(inputArray);
+
+        log.info("Large sum is : {}", largeSum);
+    }
+
+    private static int findLargestSubArray(int[] inputArray) {
+        int maxSum = inputArray[0];
+        int currentSum = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++) {
+            if (currentSum < 0) {
+                currentSum = 0; // not to carry the backlog of any -ve numbers
+            }
+            currentSum = currentSum + inputArray[i];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+}
