efd5df3108cfb153f6d15d629410fbae620cb00c
Srinath Rayabarapu <naath.r@gmail.com>
2022-12-14 11:28:55 +0530

    code refactoring

--- reactor/src/main/java/com/reactor/ReactorMain.java
+++ reactor/src/main/java/com/reactor/ReactorMain.java
@@ -1,6 +1,7 @@
 package com.reactor;
 
 import com.reactor.services.FluxServices;
+import com.reactor.services.MonoServices;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
@@ -9,13 +10,14 @@ public class ReactorMain {
     public static void main(String[] args) {
 
         FluxServices fluxServices = new FluxServices();
+        MonoServices monoServices = new MonoServices();
 
         log.info("From Flux");
         Flux<String> stringFlux = fluxServices.fruitsFlux();
         stringFlux.subscribe(fruit -> log.info(fruit));
 
         log.info("From Mono");
-        fluxServices.mangoMono().subscribe(fruit -> log.info(fruit));
+        monoServices.mangoMono().subscribe(fruit -> log.info(fruit));
 
     }
 
